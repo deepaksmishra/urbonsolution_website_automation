0b57140895a92df445a878f2b329dcf1d2c8f194
Deepak Mishra <deepak123.dm98@gmail.com>
2023-05-24 12:48:13 +0530

    Navigation & Alert Box Automation Scripts Implemented

---
+++ urbonsolution_website_automation/Navigation/NavigationLinkCheck.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace urbonsolution_website_automation.Navigation
+{ 
+class NavigationLinkTest
+    {
+        static void Main()
+        {
+            // Set up the Selenium WebDriver
+            IWebDriver driver = new ChromeDriver();
+
+            // Navigate to the webpage
+            driver.Navigate().GoToUrl("https://www.urbonsolution.com/");
+
+            // Find all the navigation links on the page
+            var navigationLinks = driver.FindElements(By.CssSelector("#menu-main-menu > li.menu-item.menu-item-type-custom.menu-item-object-custom.menu-item-has-children.menu-item-2030 > ul > li > a"));
+
+            // Iterate through each link and test navigation
+            foreach (var link in navigationLinks)
+            {
+                // Get the link's URL
+                string linkUrl = link.GetAttribute("href");
+
+                // Click the link
+                link.Click();
+
+                // Get the current URL after navigation
+                string currentUrl = driver.Url;
+
+                // Check if the navigation was successful
+                if (currentUrl == linkUrl)
+                {
+                    Console.WriteLine($"Navigation to {linkUrl} was successful!");
+                }
+                else
+                {
+                    Console.WriteLine($"Navigation to {linkUrl} failed. Current URL: {currentUrl}");
+                }
+
+                // Navigate back to the original page
+                driver.Navigate().Back();
+            }
+
+            // Quit the WebDriver
+            driver.Quit();
+        }
+    }
+
+}
---
+++ urbonsolution_website_automation/TestScripts/AlertBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using static urbonsolution_website_automation.GlobalMessage.Message;
+
+
+namespace urbonsolution_website_automation.TestScripts
+{
+    class EntryPoint
+    {
+
+        static IWebDriver driver = new ChromeDriver();
+        static IAlert alert;
+        static IWebElement Image;
+
+
+        static void Main()
+        {
+            string url = "https://www.urbonsolution.com/";
+            driver.Navigate().GoToUrl(url);
+            alert = driver.SwitchTo().Alert();
+            alert.Accept();
+            Image = driver.FindElement(By.CssSelector("body > div.sgpb-basic-bottom-left.sgpb-floating-button.sg-popup-id-5517"));
+            if (Image.Displayed)
+            {
+                SucessMessage("FirstName Element by ID Found successfully!!!");
+
+            }
+            else
+            {
+                FailMessage("No Response!!");
+            }
+            Console.WriteLine(alert.Text);
+            Thread.Sleep(2300);
+            driver.Quit();
+
+
+        }
+
+    }
+}
+
